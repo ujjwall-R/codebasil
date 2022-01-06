9410fee999834ada8142052c586756293eec72d7
mySpaceHere123 <ujjwal.raj.5798@gmail.com>
2022-01-06 22:38:24 +0530

    update profile backend

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -46,4 +46,37 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
+//@description Update user profile
+//@route PUT /users/profile
+//@access Private
+router.put("/users/profile", async (req, res) => {
+  try {
+    // console.log(req.body._id);
+    const user = await User.findById(req.body._id);
+
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.codechefUsername =
+        req.body.codechefUsername || user.codechefUsername;
+
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+
+      const updatedUser = await user.save();
+      res.json({ updatedUser });
+    } else {
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ error });
+  }
+});
+
+//@description Logout user
+//@route POST /users/logout
+//@access Private
+
 export default router;

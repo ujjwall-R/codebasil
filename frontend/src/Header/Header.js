091c3550377f38a39e097517477083bb0ab4a462
Aditya Sinha <twilight@Twilight.local>
2022-01-09 00:11:29 +0530

    done with header

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
 import "./Header.css";
 import Profile from "../assets/aditya.jpg";
+import { startSession } from "mongoose";
 const Header = (props) => {
   const [style, setStyle] = useState("flase");
   
@@ -8,6 +9,13 @@ const Header = (props) => {
   
     setStyle(!style);
   };
+
+  let stars = [];
+  for(let i = 0; i<5; i++) {
+    stars.push(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-star-fill" viewBox="0 0 16 16">
+    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+  </svg>)
+  }
 
   return (
     <Fragment>
@@ -33,6 +41,13 @@ const Header = (props) => {
               <a href="index.html">{props.name}</a>
             </h1>
             <h3 className="text-center mt-5"><a href="#">CODECHEF</a></h3>
+            <div className="text-center">{stars}</div>
+
+            <h3 className="text-center mt-5"><a href="#">CODEFORCES</a></h3>
+            <div className="text-center">{stars}</div>
+
+            <h3 className="text-center mt-5"><a href="#">HACKERRANK</a></h3>
+            <div className="text-center">{stars}</div>
           </div>
         </div>
       </header>
